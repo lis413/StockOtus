e1b6cdcf92029482913735146050b5e3510e8ffa
lis154 <lis154@mail.ru>
2022-01-11 22:12:54 +0300

    otus

--- src/main/java/ru/lis154/stockotus/StockOtusApplication.java
+++ src/main/java/ru/lis154/stockotus/StockOtusApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-
 public class StockOtusApplication {
 
 	public static void main(String[] args) {
